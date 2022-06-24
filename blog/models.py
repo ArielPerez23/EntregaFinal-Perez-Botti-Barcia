a0d7adccb740b3a743d7e38348124d1b7781393d
Ariel <ariel.botti@outlook.com>
2022-06-23 21:45:04 -0300

    Add Category Model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
+from tabnanny import verbose
 from django.db import models
+from django.utils.translation import gettext as _
 
-# Create your models here.
+class TimestampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class Category(TimestampedModel, models.Model):
+    name = models.CharField(max_length=100, blank=False, null=False)
+    state = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = _('Category')
+        verbose_name_plural = _('Categories')
+
+    def __str__(self):
+        return self.name 
