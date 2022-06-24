7acb2bfbad8d434b136612e60b0abe15ac0916c7
ArielPerez23 <arielperez2311@gmail.com>
2022-06-24 00:34:40 -0300

    Agregando views y urls

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def inicio(request):
+    
+    return render(request, "blog/index.html")
+
+def software(request):
+    
+    return render(request, "blog/software.html")
+
+def hardware(request):
+    
+    return render(request, "blog/hardware.html")
+
+def curiosidades(request):
+    
+    return render(request, "blog/curiosidades.html")
+
