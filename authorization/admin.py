67ada0979969ba2b5d6ff2ee6a9b6715ef90ba87
Ariel <ariel.botti@outlook.com>
2022-07-16 10:51:52 -0300

    User Model Update + base.html

--- authorization/admin.py
+++ authorization/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from authorization.models import User
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('id','email')
+    list_display = ('id','email', 'first_name', 'last_name')
     
 
 admin.site.register(User, UserAdmin)
