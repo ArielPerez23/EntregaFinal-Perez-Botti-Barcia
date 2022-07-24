5557a586bec0cbbaeffde1e18c5d9abd157ea6f0
andresbarcia <andresbarcia7@gmail.com>
2022-07-24 11:55:35 -0300

    Funciona la edicion de perfil - Correciones de codigo

--- blog/urls.py
+++ blog/urls.py
@@ -1,22 +1,23 @@
 from django.urls import path
 from .views import *
+from authorization.views import *
 
 urlpatterns = [
     #URLS BLOG
+    path('editarperfil/', editar_perfil, name="editarperfil"),
+    path('agregar_avatar/', agregar_avatar, name="agregar_avatar"),
+
     path('software/', software, name= "software"),
     path('hardware/', hardware, name= "hardware"),
     path('curiosidades/', curiosidades, name= "curiosidades"),
+    path('about/', about, name="about"),
     
     path('nuevo_articulo/', nuevo_articulo, name="nuevo_articulo"),
     path('eliminar_articulo/<articulo_id>', eliminar_articulo, name="eliminar_articulo"),
     path('editar_articulo/<articulo_id>', editar_articulo, name="editar_articulo"),
 
-    path('post/<int:pk>', ArticleDetailView.as_view(), name='post_detail'),
-
-    path('about/', about, name="about"),
-    path('editarperfil/', editar_perfil, name="editarperfil"),
+    #path('post/<int:pk>', ArticleDetailView.as_view(), name='post_detail'), 
     path('post/<int:pk>', ArticleDetailView.as_view(), name='post_detail'),
     path('mensajes/', MessageView.as_view(), name='messages'),
-    path('like/<int:pk>', LikeView, name='like_post'),
-    
+    path('like/<int:pk>', LikeView, name='like_post'),  
 ]
