55629f30e792f4feac2d4ed87ed03ae6e88defb1
Ariel <ariel.botti@outlook.com>
2022-07-25 07:41:14 -0300

    add labels

--- authorization/forms.py
+++ authorization/forms.py
@@ -19,12 +19,12 @@ class UserRegisterForm(UserCreationForm):
 class UserEditForm(UserChangeForm):
     email = forms.EmailField(max_length=100, widget=forms.TextInput(attrs={'class':'form-control'}))
     username = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class':'form-control'}))
-    first_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class':'form-control col-md-6'}))
-    last_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class':'form-control col-md-6'}))
-    last_login = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class':'form-control'}))
-    is_superuser = forms.CharField(max_length=100, widget=forms.CheckboxInput(attrs={'class':'form-check'}))
-    is_active = forms.CharField(max_length=100, widget=forms.CheckboxInput(attrs={'class':'form-check'}))
-    date_joined = forms.CharField(max_length=100,widget=forms.TextInput(attrs={'class':'form-control'}))
+    first_name = forms.CharField(max_length=100, label='Nombre', widget=forms.TextInput(attrs={'class':'form-control col-md-6'}))
+    last_name = forms.CharField(max_length=100, label='Apellido', widget=forms.TextInput(attrs={'class':'form-control col-md-6'}))
+    last_login = forms.CharField(max_length=100, label='Ultimo Ingreso', widget=forms.TextInput(attrs={'class':'form-control'}))
+    is_superuser = forms.CharField(max_length=100, label='Administrador', widget=forms.CheckboxInput(attrs={'class':'form-check'}))
+    is_active = forms.CharField(max_length=100, label='Usuario Activo', widget=forms.CheckboxInput(attrs={'class':'form-check'}))
+    date_joined = forms.CharField(max_length=100, label='Fecha de Alta',widget=forms.TextInput(attrs={'class':'form-control'}))
     class Meta:
         model = User
         fields = ('username', 'email', 'first_name', 'last_name', 'last_login', 'is_superuser','is_active', 'date_joined')
